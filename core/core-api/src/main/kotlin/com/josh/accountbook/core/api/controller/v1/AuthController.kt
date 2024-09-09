e184fcfbf97fcff897a404ea0443b68cb57158f0
josh <tkddns6007@gmail.com>
2024-09-09 16:19:31 +0900

    refactor: MemberController 삭제 -> AuthController 로 로직이동

--- core/core-api/src/main/kotlin/com/josh/accountbook/core/api/controller/v1/AuthController.kt
+++ core/core-api/src/main/kotlin/com/josh/accountbook/core/api/controller/v1/AuthController.kt
@@ -1,8 +1,11 @@
 package com.josh.accountbook.core.api.controller.v1
 
 import com.josh.accountbook.core.api.dto.LoginRequest
+import com.josh.accountbook.core.api.dto.SignUpRequest
 import com.josh.accountbook.core.api.support.response.ApiResponse
+import com.josh.accountbook.core.domain.member.MemberService
 import com.josh.accountbook.security.core.auth.AuthService
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,10 +14,20 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/api/v1/auth")
 class AuthController(
-    val authService: AuthService
+    val authService: AuthService,
+    val memberService: MemberService,
+    private val encoder: PasswordEncoder
 ) {
+
+    @PostMapping("/signUp")
+    fun signUp(@RequestBody signUpRequest: SignUpRequest): ApiResponse<String> {
+        signUpRequest.password = encoder.encode(signUpRequest.password)
+        return ApiResponse.success(memberService.signUp(signUpRequest.toServiceDto()))
+    }
+
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest):ApiResponse<String> {
         return ApiResponse.success(authService.login(loginRequest.toServiceDto()))
     }
+
 }
